248e32cf9f6cf5720637210bbdccc56c560f74ba
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-03-19 14:55:17 -0400

    Changed Makefile

---
+++ CPP00/ex01/srcs/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 14:46:18 by cemenjiv          #+#    #+#             */
+/*   Updated: 2023/03/19 14:53:03 by cemenjiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include "phonebook.hpp"
+#include "contact.hpp"
+
+int main()
+{
+	PhoneBook	phonebook;
+	
+	while (1)
+	{	
+		phonebook.display_phonebook_options();
+		if (phonebook.choice.compare("ADD") == 0) // Maybe I can use a switch and the case 1, case 2 , etc)
+			phonebook.ADD();
+		else if (phonebook.choice == "SEARCH")
+			phonebook.SEARCH();
+		else if (phonebook.choice == "EXIT") // We can also use "relational operators" such as "=" and "!=", but this has to be used with std::string. 
+		 	phonebook.EXIT();
+		else
+		{
+			std::cout << "\033[31mThis choice is invalid! Press ENTER to continue\033[0m" << std::endl;
+			std::cin.get();
+		}
+	}		
+	return 0;
+}
--- CPP00/ex01/srcs/phonebook.hpp
+++ CPP00/ex01/srcs/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 08:39:12 by cemenjiv          #+#    #+#             */
-/*   Updated: 2023/03/16 15:11:17 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2023/03/19 14:33:20 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@
 #include <string>
 #include "contact.hpp"
 
-// Est-ce que j'ai besoin d'un constructeur et d'un destructeur ici? 
-// In the public section, we find function members and the attributes these functions takes. How the function works will be declared in PhoneBook.cpp
-//
 class	PhoneBook{
 
 public:
 
 	PhoneBook(void);
-	//~PhoneBook(void); // Est-ce que j'ai besoin d'ajoute un destructeur 
 	std::string choice;
 	void		display_phonebook_options(void);
 	void		ADD(void);
