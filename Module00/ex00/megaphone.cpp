e2e47ec32aa87659fc6696710aa767e391b416f8
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-05-01 19:35:22 -0400

    Changed Makefile rule and change int type to size_t in for loop

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 01:35:04 by cemenjiv          #+#    #+#             */
-/*   Updated: 2023/04/13 01:35:07 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2023/05/01 19:31:11 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
 	{
 		for(int i = 1; i < argc; i++)
 		{
-			for (int j = 0; j < strlen(argv[i]); j++)
+			for (size_t j = 0; j < strlen(argv[i]); j++)
 				putchar(toupper(argv[i][j]));
 			if (argv[i + 1])
 					std::cout << ' ';
--- ModuleVideos/Module06/01-typeConversion/main.c
+++ ModuleVideos/Module06/01-typeConversion/main.c
@@ -1,34 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/03 17:29:18 by cemenjiv          #+#    #+#             */
-/*   Updated: 2023/05/01 18:46:36 by cemenjiv         ###   ########.fr       */
+/*   Created: 2023/05/01 17:11:44 by cemenjiv          #+#    #+#             */
+/*   Updated: 2023/05/01 17:15:39 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FIXED_HPP
-# define FIXED_HPP
+/* 
+Il y existe en C, 2 types de cast:
+	- Mettre en parentheses, le type de variable pour lequel on desire changer (int)4,25
+	- Cast implicite: pas besoin de rien ecrire pour qu'il ait lieu. 
+ */
 
-class Fixed{
-
-private:
-	int	_nbr; 
-	static int const _bits;
-
-public:
-	Fixed(); 
-	Fixed(Fixed const & src);
-	~Fixed();
-
-	Fixed & operator=(Fixed const & rhs);
-
-	int		getRawBits(void) const;
-	void	setRawBits(int const raw);
-};
+int main(){
+	
 
 
-#endif
+
+
+	return (0); 
+}
