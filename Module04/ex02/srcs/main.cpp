b853a20a90050311ab15d97ac7ee3d985816354f
Cesar Menjivar <cemenjiv@student.42quebec.com>
2023-05-16 21:13:47 -0400

    Changed layout of main.cpp in ex02

--- Module04/ex02/srcs/main.cpp
+++ Module04/ex02/srcs/main.cpp
@@ -6,30 +6,44 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:21:23 by cemenjiv          #+#    #+#             */
-/*   Updated: 2023/04/14 12:28:23 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2023/05/16 21:12:06 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# define RESET "\e[0m"
+# define RED "\e[0;31m"
+# define GRN "\e[0;32m"
+# define CYN "\e[0;36m"
+# define YEL "\e[0;33m"
+# define REDB "\e[41m"
 
 #include "Dog.hpp"
 #include "Cat.hpp"
 
 int main()
 {
-	//AAnimal instanciation is possible because the object it refers to is an object
+	std::cout << GRN"*************************************************************************************" RESET << std::endl;
+    std::cout << GRN"*                         INSTANCIATION OF DOG = POSSIBLE                           *" RESET << std::endl;
+    std::cout << GRN"*************************************************************************************" RESET << std::endl;
 	AAnimal* animal = new Dog();
 	animal->makeSound();
 	delete animal;
+
+	std::cout << GRN"*************************************************************************************" RESET << std::endl;
+    std::cout << GRN"*                         INSTANCIATION OF CAT = POSSIBLE                           *" RESET << std::endl;
+    std::cout << GRN"*************************************************************************************" RESET << std::endl;
 
 	AAnimal* animal1 = new Cat();
 	animal1->makeSound();
 	delete animal1;
 
-	//AAnimal instanciation IMPOSSIBLE because AAnimal is an abtract class, because one of its
-	// method is pure 
+	// std::cout << GRN"*************************************************************************************" RESET << std::endl;
+    // std::cout << GRN"*                  INSTANCIATION OF AANIMAL = IMPOSSIBLE (ABSTRACT CLASS)           *" RESET << std::endl;
+    // std::cout << GRN"*************************************************************************************" RESET << std::endl;
 
-	// AAnimal AAnimal = AAnimal();
+	// AAnimal AAnimal;
 	// AAnimal->makeSound();
-
+	
 	return (0);
 	
 }
