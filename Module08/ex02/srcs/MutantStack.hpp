b739c4f794e71b740f9fb660df8c63986e7a3ebd
Cesar Menjivar <82172640+cez20@users.noreply.github.com>
2024-04-28 21:40:51 -0400

    Added the 3 other types of iterator

--- Module08/ex02/srcs/MutantStack.hpp
+++ Module08/ex02/srcs/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 18:48:46 by cemenjiv          #+#    #+#             */
-/*   Updated: 2024/04/28 16:24:14 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2024/04/28 21:40:25 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ class MutantStack: public std::stack<T>
 {
 	public:
 		typedef	typename std::stack<T>::container_type::iterator iterator;
+		typedef	typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef	typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef	typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 		MutantStack();
 		MutantStack( MutantStack const & src );
