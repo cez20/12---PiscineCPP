0d6333ae5ed361ed87b206f3be045f25a9ce13c4
Cesar Menjivar <82172640+cez20@users.noreply.github.com>
2024-08-27 11:16:06 -0400

    Added data.csv file and created input.txt file

--- Module09/ex00/srcs/BitcoinExchange.hpp
+++ Module09/ex00/srcs/BitcoinExchange.hpp
@@ -3,20 +3,21 @@
 
 # include <iostream>
 # include <string>
+# include <map>
 
 class BitcoinExchange
 {
 
 	public:
-
-		BitcoinExchange();
+		BitcoinExchange( std::string const & );
 		BitcoinExchange( BitcoinExchange const & src );
 		~BitcoinExchange();
 
 		BitcoinExchange &		operator=( BitcoinExchange const & rhs );
 
 	private:
-
+		BitcoinExchange();
+		std::map<std::string, unsigned int> myData;
 };
 
 std::ostream &			operator<<( std::ostream & o, BitcoinExchange const & i );
