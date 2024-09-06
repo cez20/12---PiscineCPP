8d1202069c1a77759372ba359f56b63f3fff0520
Cesar Menjivar <82172640+cez20@users.noreply.github.com>
2024-09-05 22:29:11 -0400

    Added condition that program must take a database as argument

--- Module09/ex00/srcs/main.cpp
+++ Module09/ex00/srcs/main.cpp
@@ -3,11 +3,16 @@
 #include <map>
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int argc, char **argv)
 {      
+    if (argc != 2){
+        std::cerr << "ERROR! Program must contain a database as argument!" << std::endl;
+        return 1;
+    }
+    
     BitcoinExchange bitcoin_exchange = BitcoinExchange();
     std::ifstream bitcoin_database("data.csv");
-    std::ifstream second_database("input.txt");
+    std::ifstream second_database(argv[1]);
 
     // Verifying if btcDatabase is open or not 
     if (!bitcoin_database.is_open() || !second_database.is_open()){
@@ -16,10 +21,9 @@ int main()
     }
     
     bitcoin_exchange.mapBitcoinDatabase(bitcoin_database);
-    // bitcoin_exchange.printMap();
+    bitcoin_exchange.printMap();
     bitcoin_exchange.findBitcoinData(second_database);
     
-
     bitcoin_database.close();
     second_database.close();
 
