991cc34091a852c9ea8e68c98f5c9bc57f0b129f
Cesar Menjivar <82172640+cez20@users.noreply.github.com>
2024-09-11 13:24:41 -0400

    Modified original class to make it fit inside a RPN class

--- Module09/ex01/srcs/RPN.cpp
+++ Module09/ex01/srcs/RPN.cpp
@@ -4,12 +4,16 @@
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
-RPN::RPN()
-{
+RPN::RPN() {}
+
+RPN::RPN(std::string s): _mathString(s){
+
 }
 
-RPN::RPN( const RPN & src )
-{
+
+RPN::RPN( const RPN & src ) {
+
+  *this = src;
 }
 
 
@@ -28,23 +32,73 @@ RPN::~RPN()
 
 RPN &				RPN::operator=( RPN const & rhs )
 {
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
+	if ( this != &rhs )
+	{
+		this->_total = rhs._total;
+    this->_mathString = rhs._mathString; //TODO: Put getter for this as it is cleaner
+    this->_mystack = rhs._mystack;
+	}
 	return *this;
 }
-
-std::ostream &			operator<<( std::ostream & o, RPN const & i )
-{
-	//o << "Value = " << i.getValue();
-	return o;
-}
-
 
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
+
+bool RPN::isOperator(char c){
+
+  if(c == '+' || c == '-' || c == '*' || c == '/')
+    return true;
+  return false;
+}
+
+void	RPN::parse(){
+
+      _mathString.erase(std::remove_if(_mathString.begin(), _mathString.end(), ::isspace), _mathString.end()); //TODO: Fix the problem of ::isspace vs std::isspace
+      
+      int operand1 = 0;
+      int operand2 = 0;
+
+      for (size_t i = 0; i != _mathString.size(); ++i){
+        if(isOperator(_mathString[i])){
+          operand2 = _mystack.top();
+          _mystack.pop();
+          operand1 = _mystack.top();
+          _mystack.pop();
+          _total = calculate(operand1, operand2, _mathString[i]);
+          _mystack.push(_total);
+        }
+        else {
+          _mystack.push(_mathString[i] - '0');
+        }
+      }
+      std::cout << "The total of the RNP is: " << _total << std::endl;
+} 
+
+
+int RPN::calculate(int operand1, int operand2, char op){
+
+    int total;
+
+    switch(op) {
+      case '+':
+        total = operand1 + operand2;
+        break;
+      case '-':
+        total = operand1 - operand2;
+        break;
+      case '*':
+        total = operand1 * operand2;
+        break;
+      case '/':
+        total = operand1 / operand2;
+        break;
+      default:
+        std::cerr << "Error: Invalid operator" << std::endl;
+        return 0;
+    }
+    return total;  
+}
 
 
 /*
--- Module09/ex01/srcs/RPN.hpp
+++ Module09/ex01/srcs/RPN.hpp
@@ -3,22 +3,30 @@
 
 # include <iostream>
 # include <string>
+# include <algorithm> // This is for std::remove_if
+# include <cctype> // For isspace? 
+# include <stack>
 
 class RPN
 {
-
 	public:
 
 		RPN();
+		RPN(std::string s);
 		RPN( RPN const & src );
 		~RPN();
 
 		RPN &		operator=( RPN const & rhs );
 
+		void		parse();
+		bool		isOperator(char c);
+		int			calculate(int operand1, int operand2 , char op);
+
 	private:
+		int				_total;
+		std::string		_mathString;
+		std::stack<int> _mystack;
 
 };
-
-std::ostream &			operator<<( std::ostream & o, RPN const & i );
 
 #endif /* ************************************************************* RPN_H */
